67f385fecbec131ea3e35f6c1dc39931dab4a2e7
Jeremy Hanmer <jeremy.hanmer@dreamhost.com>
2012-07-20 16:46:58 -0700

    first passing of status messages

--- nsmon.py
+++ nsmon.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
+# vim: set syntax=python tabstop=2 expandtab:
 
 import DNS
 import yaml
 import threading
 import socket
+import Queue
 from time import sleep
 
 conffile = open('nsconfig.yml','r')
@@ -14,22 +16,30 @@ class MonThread(threading.Thread):
   def run(self):
     print 'monitoring ' + server
     count = 0
+    status = 'OK'
     while (1):
       for domain in domains:
         try:
           r = DNS.Request(domain,qtype='A',server=server,timeout=timeout).req()
-          if r.header['status'] != 'NOERROR':
-            Error("received status of %s when attempting to query %s for NSs"%
-              (r.header['status']))
-          else:
+          if r.header['status'] == 'NOERROR':
             print server + ' OK'
+            if ( status != 'OK' ):
+              statusQueue.put('OK ' + server)
+              status = 'OK'
         except DNS.SocketError:
           print str(server) + " Errored"
+          if ( status != 'BAD' ):
+            statusQueue.put('BAD ' + server)
+            status = 'BAD'
       sleep(frequency)
       if (cycles):
         count += 1
         if ( count >= cycles ):
           break
+
+statusQueue = Queue.Queue()
+
+availableServers = nsconfig['servers']
 
 for server in nsconfig['servers']:
   print server + ' ' + str(nsconfig['servers'][server]['timeout'])
@@ -46,5 +56,14 @@ for server in nsconfig['servers']:
 
 
 while (1):
-  sleep(1)      
+  while not statusQueue.empty():
+    statusline = statusQueue.get()
+    [status,server] = statusline.split()
+    print 'processing ' + statusline
+    if ( status == 'OK' and availableServers.has_key(server)):
+      availableServers.append(server)
+      print 'Processing recovery of ' + server
+    elif ( status == 'BAD' and availableServers.has_key(server)):
+      availableServers.pop(server)
+  sleep(1)
 
