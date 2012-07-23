ece463be9c472978e808853b84be01b4048e6f8c
Jeremy Hanmer <jeremy.hanmer@dreamhost.com>
2012-07-23 12:47:56 -0700

    pep8

--- nsmon.py
+++ nsmon.py
@@ -1,69 +1,69 @@
 #!/usr/bin/env python
-# vim: set syntax=python tabstop=2 expandtab:
+# vim: set syntax=python tabstop=4444 expandtab:
 
 import DNS
 import yaml
 import threading
-import socket
 import Queue
 from time import sleep
 
-conffile = open('nsconfig.yml','r')
+conffile = open('nsconfig.yml', 'r')
 cycles = 0
 nsconfig = yaml.load(conffile)
 
+
 class MonThread(threading.Thread):
-  def run(self):
-    print 'monitoring ' + server
+    def run(self):
+        print 'monitoring ' + server
     count = 0
     status = 'OK'
     while (1):
-      for domain in domains:
-        try:
-          r = DNS.Request(domain,qtype='A',server=server,timeout=timeout).req()
-          if r.header['status'] == 'NOERROR':
-            print server + ' OK'
-            if ( status != 'OK' ):
-              statusQueue.put('OK ' + server)
-              status = 'OK'
-        except DNS.SocketError:
-          print str(server) + " Errored"
-          if ( status != 'BAD' ):
-            statusQueue.put('BAD ' + server)
-            status = 'BAD'
-      sleep(frequency)
-      if (cycles):
-        count += 1
-        if ( count >= cycles ):
-          break
-
+        for domain in domains:
+            try:
+                r = DNS.Request(domain, qtype='A',
+                                server=server,
+                                timeout=timeout).req()
+                if r.header['status'] == 'NOERROR':
+                    print server + ' OK'
+                if (status != 'OK'):
+                    stusQueue.put('OK ' + server)
+                    status = 'OK'
+            except DNS.SocketError:
+                print str(server) + " Errored"
+                if (status != 'BAD'):
+                    statusQueue.put('BAD ' + server)
+                    status = 'BAD'
+        sleep(frequency)
+        if (cycles):
+            count += 1
+            if (count >= cycles):
+                break
 statusQueue = Queue.Queue()
 
 availableServers = nsconfig['servers']
 
 for server in nsconfig['servers']:
-  print server + ' ' + str(nsconfig['servers'][server]['timeout'])
-  domains = nsconfig['testdomains']
-  frequency = nsconfig['frequency']
-  try:
-    cycles = nsconfig['cycles']
-  except KeyError:
-    pass
-  timeout = nsconfig['servers'][server]['timeout']
-  thread = MonThread()
-  thread.daemon=True
-  thread.start()
+    print server + ' ' + str(nsconfig['servers'][server]['timeout'])
+    domains = nsconfig['testdomains']
+    frequency = nsconfig['frequency']
+    try:
+        cycles = nsconfig['cycles']
+    except KeyError:
+        pass
+    timeout = nsconfig['servers'][server]['timeout']
+    thread = MonThread()
+    thread.daemon = True
+    thread.start()
 
 
 while (1):
-  while not statusQueue.empty():
-    statusline = statusQueue.get()
-    [status,server] = statusline.split()
+    while not statusQueue.empty():
+        statusline = statusQueue.get()
+    [status, server] = statusline.split()
     print 'processing ' + statusline
-    if ( status == 'OK' and availableServers.has_key(server)):
-      availableServers.append(server)
-      print 'Processing recovery of ' + server
-    elif ( status == 'BAD' and availableServers.has_key(server)):
-      availableServers.pop(server)
-  sleep(1)
-
+    if (status == 'OK' and server in availableServers):
+        availableServers.append(server)
+        print 'Processing recovery of ' + server
+    elif (status == 'BAD' and server in availableServers):
+        availableServers.pop(server)
+    sleep(1)
